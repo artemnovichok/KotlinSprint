3faa4cda3cf962c5465cf57f6fee181b6f3fffb8
artemnovichok <88651599+artemnovichok@users.noreply.github.com>
2025-12-02 15:26:28 +0300

    finis

--- src/main/kotlin/firstLesson/task-3.kt
+++ src/main/kotlin/firstLesson/task-3.kt
@@ -1,2 +1,24 @@
 package org.example.firstLesson
 
+fun main()
+{
+    val yearOfFlight: UShort = 1961u;
+    var hourData: UByte;
+    var minuteData: UByte;
+
+    //take-off
+    hourData = 9u;
+    minuteData = 7u;
+
+    println(yearOfFlight);
+    println(hourData);
+    println(minuteData);
+
+    //boarding
+    hourData = 10u;
+    minuteData = 55u;
+
+    print(hourData);
+    print(':');
+    print(minuteData);
+}
