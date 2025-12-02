0bfc59970226a7da88473e6d0730be45f496ad16
artemnovichok <88651599+artemnovichok@users.noreply.github.com>
2025-12-02 15:58:02 +0300

    finish tasck 4

---
+++ src/main/kotlin/firstLesson/task-4.kt
@@ -0,0 +1,11 @@
+package org.example.firstLesson
+
+fun main()
+{
+    val length: ULong = 40_868_600_000u;
+    val age: UByte = 27u;
+    val partOfDay: Float = 0.075f;
+    val seconds: UShort = 6480u;
+    val partOfYear: Double = 2.0547945205479453E-4;
+    val apogee: UInt = 327000u;
+}
