174bc8ff4f59e1787a1ce8a9b127c99dd4de7967
artemnovichok <88651599+artemnovichok@users.noreply.github.com>
2025-12-02 14:48:55 +0300

    add info

--- src/main/kotlin/firstLesson/lesson.kt
+++ src/main/kotlin/firstLesson/lesson.kt
@@ -2,8 +2,8 @@ package org.example.firstLesson
 
 fun main()
 {
-    var number = 42; //edit
-    val someString = "Some str"; //not edit
+    var number: Int = 42; //edit
+    val someString: String = "Some str"; //not edit
 
     println(number)
 
@@ -12,4 +12,21 @@ fun main()
     number +=  10
     println(number)
 
+    println("------------")
+    //types
+
+    val intNum1: Int = -2147483648; //32 bit
+    val intNum2: Int = 2147483647;
+    val longNum: Long; //64 bit
+    val shortNum: Short; //16 bit
+    val byteNum: Byte; //8 bit
+    val plusNum: UInt; //32 bit of plus nums from Int
+
+    val doubleNum: Double = 1.3;  //64 bit
+    val floatNum: Float = 1.3f; // 32 bit
+
+    val stringValue: String = "Test";  // " " - is important
+    val chrValue: Char = 'A'; // ' ' - is important
+
+    val boolValue: Boolean = true; // false and true
 }
