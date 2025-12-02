16c63079e44c96253bbfebeea54f5b17726e22cb
artemnovichok <88651599+artemnovichok@users.noreply.github.com>
2025-12-02 15:07:43 +0300

    before dismissal

--- src/main/kotlin/firstLesson/task-2.kt
+++ src/main/kotlin/firstLesson/task-2.kt
@@ -1,2 +1,13 @@
 package org.example.firstLesson
 
+fun main()
+{
+    val numberOfOrders: UShort = 72u;
+    val welcomeMessage: String = "Thanks for your order!";
+
+    println(numberOfOrders)
+    println(welcomeMessage)
+
+    val numberOfWorkers: UShort = 2000u;
+    println(numberOfWorkers)
+}
