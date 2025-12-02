ef11232df7ca0a394df16f661ce1f5cb09dfe755
artemnovichok <88651599+artemnovichok@users.noreply.github.com>
2025-12-02 20:35:27 +0300

    fix

--- src/main/kotlin/firstLesson/task-1.kt
+++ src/main/kotlin/firstLesson/task-1.kt
@@ -2,6 +2,6 @@ package org.example.firstLesson
 
 fun main()
 {
-    val numberOfOrders: UShort = 72u;
+    val numberOfOrders: Byte = 72;
     val welcomeMessage: String = "Thanks for your order!";
 }
