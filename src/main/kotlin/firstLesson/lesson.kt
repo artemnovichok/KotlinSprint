67889a1803ca98fb8046dc8961533cca7347c4c5
artemnovichok <88651599+artemnovichok@users.noreply.github.com>
2025-12-02 14:39:22 +0300

    add lesson script

---
+++ src/main/kotlin/firstLesson/lesson.kt
@@ -0,0 +1,15 @@
+package org.example.firstLesson
+
+fun main()
+{
+    var number = 42; //edit
+    val someString = "Some str"; //not edit
+
+    println(number)
+
+    println(someString)
+
+    number +=  10
+    println(number)
+
+}
