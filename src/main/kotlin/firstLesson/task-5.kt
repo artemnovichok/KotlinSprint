e8297a16df5806d96cbbfff679440b8068393532
artemnovichok <88651599+artemnovichok@users.noreply.github.com>
2025-12-02 21:15:18 +0300

    fix

--- src/main/kotlin/firstLesson/task-5.kt
+++ src/main/kotlin/firstLesson/task-5.kt
@@ -1,16 +1,19 @@
 package org.example.firstLesson
 
+const val SECOND_IN_MINUTE: Byte = 60;
+const val MINUTE_IN_HOUR: Byte = 60;
+
 fun main()
 {
-    val totalSeconds: UInt = 6000u;
+    val totalSeconds: Int = 6000;
+    val hours:Int = totalSeconds / SECOND_IN_MINUTE / MINUTE_IN_HOUR;
+    val minutes: Int = (totalSeconds - hours * MINUTE_IN_HOUR * SECOND_IN_MINUTE ) / SECOND_IN_MINUTE
+    val seconds: Int = totalSeconds - hours * MINUTE_IN_HOUR * SECOND_IN_MINUTE - minutes * SECOND_IN_MINUTE
 
-    val hours:UInt = totalSeconds / 60u / 60u;
-    val minutes: UInt = (totalSeconds - hours * 60u * 60u ) / 60u
-    val seconds: UInt = totalSeconds - hours * 60U * 60u - minutes * 60u
+    print("${formatToTwoChars(hours)}:${formatToTwoChars(minutes)}:${formatToTwoChars(seconds)}")
+}
 
-    print(hours)
-    print(':')
-    print(minutes)
-    print(':')
-    println(seconds)
+fun formatToTwoChars(number: Int) : String
+{
+    return "%02d".format(number);
 }
