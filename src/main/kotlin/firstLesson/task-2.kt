548601cc7447ec2a460cbb3e0fef8de4c197671f
artemnovichok <88651599+artemnovichok@users.noreply.github.com>
2025-12-02 20:38:55 +0300

    fix

--- src/main/kotlin/firstLesson/task-2.kt
+++ src/main/kotlin/firstLesson/task-2.kt
@@ -2,15 +2,15 @@ package org.example.firstLesson
 
 fun main()
 {
-    val numberOfOrders: UShort = 72u;
+    val numberOfOrders: Byte = 72;
     val welcomeMessage: String = "Thanks for your order!";
 
     println(numberOfOrders);
     println(welcomeMessage);
 
-    var numberOfWorkers: UShort = 2000u;
+    var numberOfWorkers: Short = 2000;
 //    println(numberOfWorkers)
 
-    numberOfWorkers = (numberOfWorkers - 1u).toUShort();
+    numberOfWorkers = (numberOfWorkers - 1).toShort();
     println(numberOfWorkers);
 }
