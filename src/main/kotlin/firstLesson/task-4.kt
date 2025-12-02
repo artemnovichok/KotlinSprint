01ad874009b8b2bad981dda6b19d700eb71c3c3a
artemnovichok <88651599+artemnovichok@users.noreply.github.com>
2025-12-02 20:41:24 +0300

    fix

--- src/main/kotlin/firstLesson/task-4.kt
+++ src/main/kotlin/firstLesson/task-4.kt
@@ -2,10 +2,10 @@ package org.example.firstLesson
 
 fun main()
 {
-    val length: ULong = 40_868_600_000u;
-    val age: UByte = 27u;
+    val length: Long = 40_868_600_000;
+    val age: Byte = 27;
     val partOfDay: Float = 0.075f;
-    val seconds: UShort = 6480u;
+    val seconds: Short = 6480;
     val partOfYear: Double = 2.0547945205479453E-4;
-    val apogee: UInt = 327000u;
+    val apogee: Int = 327000;
 }
