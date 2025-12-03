7112ed00ac1640c90fc2671d14822ce86bc48194
artemnovichok <88651599+artemnovichok@users.noreply.github.com>
2025-12-03 19:33:45 +0300

    finish

--- src/main/kotlin/secondLesson/task5.kt
+++ src/main/kotlin/secondLesson/task5.kt
@@ -1,2 +1,17 @@
 package org.example.secondLesson
 
+import kotlin.math.pow
+
+const val PERCENT = 16.7
+fun main()
+{
+    val contribution: Int = 70_000
+    val years: Byte = 20
+    println(threeChars(contribution * (1 + PERCENT * 0.01).toDouble().pow(years.toDouble())))
+}
+
+
+fun threeChars(number: Double):String
+{
+    return "%.3f".format(number)
+}
