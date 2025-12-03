06ff818f7913b650a45f47056eebdf941fe42dd2
artemnovichok <88651599+artemnovichok@users.noreply.github.com>
2025-12-04 00:58:57 +0300

    finish task 5

---
+++ src/main/kotlin/FourLesson/Lesson.kt
@@ -0,0 +1,12 @@
+package org.example.FourLesson
+
+const val MIN_AGE = 18
+const val MAX_AGE = 30
+
+fun main()
+{
+    // &&, ||, !
+    val age:Byte = 23
+    val result = age in MIN_AGE..MAX_AGE
+    //value1..value2
+}
