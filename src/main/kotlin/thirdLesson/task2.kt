0b35754219697a0f8448548e6bf7494957962d61
artemnovichok <88651599+artemnovichok@users.noreply.github.com>
2025-12-03 22:06:01 +0300

    finish task 2

--- src/main/kotlin/thirdLesson/task2.kt
+++ src/main/kotlin/thirdLesson/task2.kt
@@ -1,2 +1,24 @@
 package org.example.thirdLesson
 
+fun main()
+{
+    //like struct of user data from db
+    var age: Byte = 10
+    var lastName: String = "Андреева"
+    val firstName: String = "Татьяна"
+    val surname: String = "Сергеевна"
+
+    println(ToFioAgeFormat(lastName, firstName, surname, age))
+
+    //age 22
+    age = 22
+    lastName = "Сидорова"
+
+    println(ToFioAgeFormat(lastName, firstName, surname, age))
+
+}
+
+fun ToFioAgeFormat(lastName: String, firstName: String, surname: String, age: Byte) : String
+{
+    return "$lastName $firstName $surname, $age"
+}
