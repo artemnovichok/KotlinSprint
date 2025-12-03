66463f268c72d54da509a02afe01529202725e3c
artemnovichok <88651599+artemnovichok@users.noreply.github.com>
2025-12-03 16:06:26 +0300

    finish task 2

--- src/main/kotlin/secondLesson/task2.kt
+++ src/main/kotlin/secondLesson/task2.kt
@@ -1,2 +1,16 @@
 package org.example.secondLesson
 
+fun main()
+{
+    val workers: Short = 50
+    val workerSalary: Short = 30_000
+    val interns: Short = 30
+    val internSalary: Short = 20_000
+
+    val workersTotalExpenses: Int = workers * workerSalary
+    val totalExpenses: Int = workersTotalExpenses + interns * internSalary
+    val averageSalary: Float = totalExpenses.toFloat() / (workers + interns)
+    println(workersTotalExpenses)
+    println(totalExpenses)
+    println(averageSalary)
+}
