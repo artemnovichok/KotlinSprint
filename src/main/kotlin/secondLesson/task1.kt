49472626b594002d2584f83f369d95904fe96d6f
artemnovichok <88651599+artemnovichok@users.noreply.github.com>
2025-12-03 15:56:02 +0300

    finish task 1

--- src/main/kotlin/secondLesson/task1.kt
+++ src/main/kotlin/secondLesson/task1.kt
@@ -1,2 +1,9 @@
 package org.example.secondLesson
 
+fun main()
+{
+    println("Ученик 1: " + "%.2f".format((3 + 4 + 3 + 5) / 4f))
+    println("Ученик 2: " + "%.2f".format((3 + 4 + 3 + 5) / 4f))
+    println("Ученик 3: " + "%.2f".format((3 + 4 + 3 + 5) / 4f))
+    println("Ученик 4: " + "%.2f".format((3 + 4 + 3 + 5) / 4f))
+}
