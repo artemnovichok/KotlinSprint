5100bc2432c0f3a833b662a9c2f8f20ead9451e3
artemnovichok <88651599+artemnovichok@users.noreply.github.com>
2025-12-03 21:46:10 +0300

    finish task 1

--- src/main/kotlin/thirdLesson/task1.kt
+++ src/main/kotlin/thirdLesson/task1.kt
@@ -1,2 +1,23 @@
 package org.example.thirdLesson
 
+fun main()
+{
+    val userName: String = "Artem"
+    var helloText: String
+
+    //day
+
+    helloText = """
+            Good day, $userName!
+                Wait a moment.
+    """.trimIndent()
+
+    println(helloText)
+
+    helloText = """
+            Good evening, $userName!
+                Wait a moment.
+    """.trimIndent()
+
+    println(helloText)
+}
