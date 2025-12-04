8c59f96a95fa1372cf097930b59011e2f3f40a50
artemnovichok <88651599+artemnovichok@users.noreply.github.com>
2025-12-04 09:00:32 +0300

    finish task 1

--- src/main/kotlin/fifthLesson/task1.kt
+++ src/main/kotlin/fifthLesson/task1.kt
@@ -1,2 +1,18 @@
 package org.example.fifthLesson
 
+fun main()
+{
+    var answer: Int? = null
+    while(answer == null)
+    {
+        println("Проверка на робота. Ответьте 2 x 2 =")
+        answer = readln().toIntOrNull()
+        if(answer == null || answer != 4)
+        {
+            answer = null
+            println("Доступ запрещен.")
+        }
+    }
+
+    println("Добро пожаловать!")
+}
