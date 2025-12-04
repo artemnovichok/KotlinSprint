9bfcfc4184277821adf38758d8a3c920435d88aa
artemnovichok <88651599+artemnovichok@users.noreply.github.com>
2025-12-04 21:38:03 +0300

    finish task 4

--- src/main/kotlin/fifthLesson/task4.kt
+++ src/main/kotlin/fifthLesson/task4.kt
@@ -2,5 +2,30 @@ package org.example.fifthLesson
 
 fun main()
 {
+    val registeredLogin = "Zaphod"
+    val registeredPassword = "PanGalactic"
 
+    println("Введите имя пользователя:")
+    val enteredLogin = readlnOrNull()
+
+    if (enteredLogin != registeredLogin) {
+        println("Пользователь не зарегистрирован. Хотите зарегистрироваться?")
+        return
+    }
+
+    println("Введите пароль:")
+    val enteredPassword = readlnOrNull()
+
+    if (enteredPassword == registeredPassword) {
+        println(
+            """
+            [вздыхает...] Ваши данные проверены, и о, чудо, они верны... 
+            Пользователь "$registeredLogin", вам разрешено входить на борт корабля "Heart of Gold". 
+            Хотя мне всё равно... Ну вперед, войдите... Если вам так уж надо, в конце концов... 
+            [меланхолический вздох...] Надеюсь, вам понравится пребывание здесь больше, чем мне.
+            """.trimIndent()
+        )
+    } else {
+        println("Пароль неверный. Доступ запрещён.")
+    }
 }
