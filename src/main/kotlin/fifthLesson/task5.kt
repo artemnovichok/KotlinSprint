f08ad9ea1cd4f927436e48320055e72efe8756fe
artemnovichok <88651599+artemnovichok@users.noreply.github.com>
2025-12-04 21:57:43 +0300

    fix

--- src/main/kotlin/fifthLesson/task5.kt
+++ src/main/kotlin/fifthLesson/task5.kt
@@ -1,2 +1,8 @@
 package org.example.fifthLesson
 
+fun main()
+{
+    val firstNumber: Int = readln().toInt()
+    val secondNumber: Int = readln().toInt()
+    val thirdNumber: Int = readln().toInt()
+}
